0d5306147cbb640bfc90c99e974a7cb369feefab
TretiakAnton <anton.v.tretyak@gmail.com>
2021-09-18 18:36:50 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo/pages/home.dart';
+void main() => runApp(MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.deepPurple,
+      ),
+      home: Home(),
+    ));
