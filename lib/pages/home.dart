dfdc1e6f48a91e2a7c35ee25b2bb0b44598c918c
TretiakAnton <anton.v.tretyak@gmail.com>
2021-09-19 13:11:52 +0300

    list added

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,58 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List todoList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    todoList.addAll(['Buy milk', 'Wash dishes']);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.cyanAccent,
+      appBar: AppBar(
+        title: Text('Список дел'),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+          itemCount: todoList.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Dismissible(
+              key: Key(todoList[index]),
+              child: Card(
+                child: ListTile(
+                  title: Text(todoList[index]),
+                  trailing: IconButton(
+                    icon: Icon(
+                      Icons.delete_forever,
+                      color: Colors.indigo,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        todoList.removeAt(index);
+                      });
+                    },
+                  ),
+                ),
+              ),
+              onDismissed: (direction) {
+                // if (direction == DismissDirection.startToEnd),
+                setState(() {
+                  todoList.removeAt(index);
+                });
+              },
+            );
+          }),
+    );
   }
 }
